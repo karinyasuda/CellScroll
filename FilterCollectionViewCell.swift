98ceea01cc85e85609f9703b3519353e90afff46
Karin <karin.03k35@gmail.com>
2016-04-07 17:07:15 +0900

    label imageview に表示出したかったけれど出せず、、、

---
+++ FilterCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FilterCollectionViewCell.swift
+//  CellScroll
+//
+//  Created by Karin on 2016/04/06.
+//  Copyright © 2016年 Karin. All rights reserved.
+//
+
+import UIKit
+
+class FilterCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
